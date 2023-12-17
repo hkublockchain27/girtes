42e7868fce36a992a8b3286bf7e81432421a71fd
Abdullah Özkoç <84963175+abdullahozkoc@users.noreply.github.com>
2023-12-17 05:05:38 +0300

    Update contract.sol
    Signed-off-by: Abdullah Özkoç <84963175+abdullahozkoc@users.noreply.github.com>

--- contracts/contract.sol
+++ contracts/contract.sol
@@ -4,23 +4,49 @@ pragma solidity ^0.8.0;
 // OpenZeppelin kütüphanesinden ERC721.sol dosyasını içe aktar
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-
 // ERC721.sol dosyasından türetilen MyNFT adlı bir akıllı sözleşme tanımla
 contract MyNFT is ERC721URIStorage {
 
+    // Akıllı sözleşmenin sahibini belirleyen bir değişken tanımla
+    address public owner;
+
+    // NFT'lerin toplam sayısını tutan bir değişken tanımla
+    uint256 public totalSupply;
+
+    // NFT'lerin metaverilerini depolayan bir harita tanımla
+    mapping(uint256 => string) public tokenURIs;
+
     // Akıllı sözleşmenin kurucu fonksiyonu
     // ERC721.sol dosyasının kurucu fonksiyonunu çağırarak, akıllı sözleşmenin adını ve sembolünü belirle
+    // Akıllı sözleşmenin sahibini, fonksiyonu çağıran hesap olarak atama
     constructor() ERC721("MyNFT", "MNFT") {
+        owner = msg.sender;
+    }
+
+    // Sadece akıllı sözleşmenin sahibinin çağırabileceği bir değiştirici tanımla
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function.");
+        _;
     }
 
     // NFT oluşturan bir fonksiyon tanımla
-    // Fonksiyonun parametreleri: NFT'nin kimliği ve metaverisi
+    // Fonksiyonun parametreleri: NFT'nin metaverisi
     // Fonksiyonun erişim belirleyicisi: public
-    function mint(uint256 tokenId, string memory tokenURI) public {
+    // Fonksiyonun değiştiricisi: onlyOwner
+    function mint(string memory tokenURI) public onlyOwner {
+        // NFT'nin kimliğini, toplam arzı artırarak belirle
+        totalSupply++;
+        uint256 tokenId = totalSupply;
+
         // Fonksiyonu çağıran hesabı, NFT'nin sahibi olarak belirle
-        address owner = msg.sender;
+        address sender = msg.sender;
+
         // ERC721.sol dosyasındaki _mint fonksiyonunu çağırarak, NFT'yi oluştur
-        _mint(owner, tokenId);
+        _mint(sender, tokenId);
+
+        // NFT'nin metaverisini haritaya ekle
+        tokenURIs[tokenId] = tokenURI;
+
         // ERC721.sol dosyasındaki _setTokenURI fonksiyonunu çağırarak, NFT'nin metaverisini belirle
         _setTokenURI(tokenId, tokenURI);
     }
@@ -31,10 +57,10 @@ contract MyNFT is ERC721URIStorage {
     function transfer(address to, uint256 tokenId) public {
         // Fonksiyonu çağıran hesabı, NFT'nin göndericisi olarak belirle
         address from = msg.sender;
-        // ERC721.sol dosyasındaki transferFrom fonksiyonunu çağırarak, NFT'yi transfer et
-        transferFrom(from, to, tokenId);
-        // NFT'nin alıcısının, jetonu başka bir kişiye atamasını engellemek için, NFT'nin sahibinin kendisi dışında kimseye transfer izni vermemesini sağla
-        _approve(address(0), tokenId, msg.sender);
 
+        // ERC721.sol dosyasındaki safeTransferFrom fonksiyonunu çağırarak, NFT'yi transfer et
+        safeTransferFrom(from, to, tokenId);
     }
 }
+
+
